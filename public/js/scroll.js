7815933a0d713e58ed1fa51fa5deaaedc1516203
devwill <wf-mietti@live.com>
2023-01-17 14:22:57 -0300

    Clone-Resposivo-Netflix 2.2

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,21 +1,19 @@
-const setupScrooling= () =>{
-  const container = [...document.querySelectorAll(".movie-container")]
+const setupScrooling = () => {
+  const container = [...document.querySelectorAll('.movie-container')]
 
-  const nextBtn = [...document.querySelectorAll(".next-btn")]
-  const prevtBtn = [...document.querySelectorAll(".pre-btn")]
+  const nextBtn = [...document.querySelectorAll('.next-btn')]
+  const prevtBtn = [...document.querySelectorAll('.pre-btn')]
 
-container.forEach((item, i)=>{
-  let containerDimensions = item.getBoundingClientRect()
-  let containerWidth = containerDimensions.width
+  container.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect()
+    let containerWidth = containerDimensions.width
 
-  nextBtn[i].addEventListener('click', ()=>{
-    item.scrollLeft += containerWidth
+    nextBtn[i].addEventListener('click', () => {
+      item.scrollLeft += containerWidth
+    })
+
+    nextBtn[i].addEventListener('click', () => {
+      item.scrollLeft -= containerWidth
+    })
   })
-
-  nextBtn[i].addEventListener('click', ()=>{
-    item.scrollLeft -= containerWidth
-  })
-
-})
-
 }
