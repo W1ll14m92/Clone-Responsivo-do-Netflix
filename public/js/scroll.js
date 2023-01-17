f8ed16baaddeac5179cc77b2496eed116c17ab1a
devwill <wf-mietti@live.com>
2023-01-17 12:46:57 -0300

    Atualização-Clone-Resposivo-Netflix 2.2

---
+++ public/js/scroll.js
@@ -0,0 +1,21 @@
+const setupScrooling= () =>{
+  const container = [...document.querySelectorAll(".movie-container")]
+
+  const nextBtn = [...document.querySelectorAll(".next-btn")]
+  const prevtBtn = [...document.querySelectorAll(".pre-btn")]
+
+container.forEach((item, i)=>{
+  let containerDimensions = item.getBoundingClientRect()
+  let containerWidth = containerDimensions.width
+
+  nextBtn[i].addEventListener('click', ()=>{
+    item.scrollLeft += containerWidth
+  })
+
+  nextBtn[i].addEventListener('click', ()=>{
+    item.scrollLeft -= containerWidth
+  })
+
+})
+
+}
